dc1c22d38019aaa456572dce0d01ff91f26ae05a
凯哥笔记 <58034319+zhang-kaipeng@users.noreply.github.com>
2020-06-07 16:17:36 +0800

    标题说明
    介绍详细信息

---
+++ java.java
@@ -0,0 +1,17 @@
+package com.hall.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AA {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("AAA3");
+        list.add("AAA4");
+        int  m = 19;
+        int n = 3;
+         System.out.println(count);
+        System.out.println(list.toString());
+
+    }
+}
